b366c490b7b846e9132d1b1eaa29c862dfed869b
tyellaineho <tyh2106@tc.columbia.edu>
2021-01-29 22:16:56 -0500

    pushing up some new markdown documentation for pybank

--- Python-PyBank/README.md
+++ Python-PyBank/README.md
@@ -1,2 +1,12 @@
 # PyBank
+
 PyBank is one of the first Python homework assignments that I completed at the Columbia University Data Analytics Bootcamp. In this assignment, I extracted a .csv file from a local path and used a for loop to iterate through financial data. The final code will print out a .txt file containing analyses of financial data.
+
+### Analysis Results
+
+The greatest increases in profits were documented in February 2012. The greatest decreases in profits is documented in September 2013.
+
+[financial-output](Images/financial-output.png)
+
+###### Technologies
+Python 3.8.5
--- Python-PyBank/main.py
+++ Python-PyBank/main.py
@@ -1,34 +1,41 @@
 import os
 import csv
+import sys
 
-csv_path = os.path.join("..","Resources", "budget_data.csv")
+csv_path = os.path.join("Resources", "budget_data.csv")
 
 with open(csv_path, newline='') as bankdata:
     bankreader = csv.reader(bankdata)
-    next(bankreader) #skip heading
+    next(bankreader)  # skip heading
 
-    bank_list= [row for row in bankreader] #accumulate your data into a list
+    bank_list = [row for row in bankreader]  # accumulate your data into a list
 
-    netprofit = [int(row[1]) for row in bank_list] #accumulate the profit into a list
+    # accumulate the profit into a list
+    netprofit = [int(row[1]) for row in bank_list]
 
-    total = sum(netprofit) #sum list into variable total
+    total = sum(netprofit)  # sum list into variable total
 
-    months = len(netprofit) #calculate months
+    months = len(netprofit)  # calculate months
 
-    profitchange = [int(netprofit[i] - netprofit[i-1]) for i in range(1,months)] #calculate the change in profit and accumulate into list
+    # calculate the change in profit and accumulate into list
+    profitchange = [int(netprofit[i] - netprofit[i-1])
+                    for i in range(1, months)]
 
-    changeaverage = sum(profitchange)/len(profitchange) #calculate average
+    changeaverage = sum(profitchange)/len(profitchange)  # calculate average
 
-    changemax = max(profitchange) #retrieve maximum change
-    changemin = min(profitchange) #retriece minimum change
+    changemax = max(profitchange)  # retrieve maximum change
+    changemin = min(profitchange)  # retriece minimum change
 
-    maxindex = profitchange.index(changemax) #retrieve index number
-    minindex = profitchange.index(changemin) #retrieve index number
+    maxindex = profitchange.index(changemax)  # retrieve index number
+    minindex = profitchange.index(changemin)  # retrieve index number
 
-    maxmonth = bank_list[int(maxindex + 1)][0] #retrieve month value and increment by 1
-    minmonth = bank_list[int(minindex + 1)][0] #retrieve month value and increment by 1
+    # retrieve month value and increment by 1
+    maxmonth = bank_list[int(maxindex + 1)][0]
+    # retrieve month value and increment by 1
+    minmonth = bank_list[int(minindex + 1)][0]
 
-    #print result to terminal
+# print result to terminal
+def print_results():
     print("Financial Analysis")
     print("----------------------------")
     print(f'Total Months: {months}')
@@ -36,13 +43,9 @@ with open(csv_path, newline='') as bankdata:
     print(f'Average Change: ${round(changeaverage,2)}')
     print(f'Greatest Increase in Profits: {maxmonth} (${changemax})')
     print(f'Greatest Decrease in Profits: {minmonth} (${changemin})')
-    #print result to .txt
-    financialanalysis = open('financialanalysis.txt', 'w')
-    print("Financial Analysis", file = financialanalysis)
-    print("----------------------------", file = financialanalysis)
-    print(f'Total Months: {months}', file = financialanalysis)
-    print(f'Total: ${total}', file = financialanalysis)
-    print(f'Average Change: ${round(changeaverage,2)}', file = financialanalysis)
-    print(f'Greatest Increase in Profits: {maxmonth} (${changemax})', file = financialanalysis)
-    print(f'Greatest Decrease in Profits: {minmonth} (${changemin})', file = financialanalysis)
-    financialanalysis.close()
+
+print_results()
+
+sys.stdout = open('financialanalysis.txt', 'w')
+print_results()
+sys.stdout.close()
