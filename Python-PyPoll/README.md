a8db271a9914bb8fa7ffdeb968b717af961668bf
tyellaineho <tyh2106@tc.columbia.edu>
2021-01-29 22:08:31 -0500

    refactoring pypoll

--- Python-PyPoll/README.md
+++ Python-PyPoll/README.md
@@ -1,2 +1,10 @@
-# PyPoll
+# Python Polls
+
 PyPoll is one of the first Python homework assignments that I completed at the Columbia University Data Analytics Bootcamp. In this assignment, I extracted a .csv file from a local path and used a for loop to iterate through polling data to determine the winner candidate. After the candidate has been selected, then the code will print out a .txt file containing analyses of polling data which includes the number and percentage of votes each candidate received.
+
+### Analysis Results
+
+The results indicate that Khan has won the election with 63% of the total votes.
+A sample of the terminal output is below:
+
+![PyPoll-Output](/Images/pypoll-output.png)
--- Python-PyPoll/main.py
+++ Python-PyPoll/main.py
@@ -1,72 +1,52 @@
 import os
 import csv
+import sys
 
-csv_path = os.path.join("..","Resources", "election_data.csv")
+csv_path = os.path.join("Resources", "election_data.csv")
 
-#`Voter ID`, `County`, and `Candidate`
+# `Voter ID`, `County`, and `Candidate`
 with open(csv_path, newline='') as polldata:
     pollreader = csv.reader(polldata)
     next(pollreader)
     poll_list = [row for row in pollreader]
-    
+
     # * The total number of votes cast
-    totalvotes = len(poll_list)
-    
+    total_votes = len(poll_list)
+
     # * A complete list of candidates who received votes
-    candidates =[row[2] for row in poll_list]
-    unique = set(candidates)
-    candidate_list = list(unique)
-    
+    candidate_list = list(set([cand[2] for cand in poll_list]))
+
     # * The total number of votes each candidate won
-    khanct = [row for row in poll_list if row[2] == "Khan"]
-    correyct = [row for row in poll_list if row[2] == "Correy"]
-    lict = [row for row in poll_list if row[2] == "Li"]
-    otooleyct = [row for row in poll_list if row[2] == "O'Tooley"]
+    vote_dict = {}
+    for candidate in candidate_list:
+        candidate_votes = len(
+            [row for row in poll_list if row[2] == candidate])
+        vote_dict.update({candidate: candidate_votes})
 
-    khantot = len(khanct)
-    correytot = len(correyct)
-    litot = len(lict)
-    otooleytot = len(otooleyct)
-    
     # * The percentage of votes each candidate won
-    khanpct = (len(khanct) / totalvotes)*100
-    correypct = (len(correyct) / totalvotes)*100
-    lipct = (len(lict) / totalvotes)*100
-    otooleypct = (len(otooleyct) / totalvotes)*100
-    
+    pct_dict = {}
+    for candidate in candidate_list:
+        candidate_pct = round((vote_dict.get(candidate)/total_votes)*100, 2)
+        pct_dict.update({candidate: candidate_pct})
+
     # * The winner of the election based on popular vote.
-    
-    if khantot >= correytot or litot or otooleytot:
-        win = "Khan"
-    elif correytot >= khantot or litot or otooleytot:
-        win = "Correy"
-    elif litot >= khantot or correytot or otooleytot:
-        win = "Li"
-    elif otooleytot >= khantot or correytot or litot:
-        win = "O'Tooley"
+    for key, value in pct_dict.items():
+        if value == max(pct_dict.values()):
+            winner = key
+            
+# #print result to terminal and .txt
+def print_results():
+    print("Election Results")
+    print("----------------------------")
+    print(f'Total Votes: {total_votes} ')
+    print("----------------------------")
+    for candidate in candidate_list:
+        print(f"{candidate} : {vote_dict.get(candidate)} votes ({pct_dict.get(candidate)}%)")
+    print("----------------------------")
+    print(f'Winner: {winner}')
 
-#print result to terminal
-print("Election Results")
-print("----------------------------")
-print(f'Total Votes: {totalvotes} ')
-print("----------------------------")
-print(f'Khan: {round(khanpct,3)}00% ({khantot})')
-print(f'Correy: {round(correypct,3)}00% ({correytot})')
-print(f'Li: {round(lipct,3)}00% ({litot})')
-print(f"O'Tooley: {round(otooleypct,3)}00% ({otooleytot})")
-print("----------------------------")
-print(f'Winner: {win}')
+print_results()
 
-#printed results to txt
-electionfile = open('electionresults.txt', 'w')
-print("Election Results", file = electionfile)
-print("----------------------------", file = electionfile)
-print(f'Total Votes: {totalvotes}', file = electionfile)
-print("----------------------------")
-print(f'Khan: {round(khanpct,3)}00% ({khantot})', file = electionfile)
-print(f'Correy: {round(correypct,3)}00% ({correytot})', file = electionfile)
-print(f'Li: {round(lipct,3)}00% ({litot})', file = electionfile)
-print(f"O'Tooley: {round(otooleypct,3)}00% ({otooleytot})", file = electionfile)
-print("----------------------------", file = electionfile)
-print(f'Winner: {win}', file = electionfile)
-electionfile.close()
+sys.stdout = open('electionresults.txt', 'w')
+print_results()
+sys.stdout.close()
